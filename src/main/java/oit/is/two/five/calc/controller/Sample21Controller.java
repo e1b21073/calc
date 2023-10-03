5ec7e881a660629a32dd1ca0bdc4b7f003ba0cd6
koichi-shirakashi <e1b21050@oit.ac.jp>
2023-10-03 16:30:08 +0900

    課題2-1

---
+++ src/main/java/oit/is/two/five/calc/controller/Sample21Controller.java
@@ -0,0 +1,43 @@
+package oit.is.two.five.calc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * Sample21Controller
+ *
+ * クラスの前に@Controllerをつけていると，HTTPリクエスト（GET/POSTなど）があったときに，このクラスが呼び出される
+ */
+@Controller
+public class Sample21Controller {
+
+  /**
+   * sample21というGETリクエストがあったら sample21()を呼び出し，sample21.htmlを返す
+   */
+  @GetMapping("/sample21")
+  public String sample21() {
+    return "sample21.html";
+  }
+
+  @GetMapping("/sample24")
+  public String sample24() {
+    return "sample24.html";
+  }
+
+  /**
+   * パスパラメータ2つをGETで受け付ける 1つ目の変数をparam1という名前で，2つ目の変数をparam2という名前で受け取る
+   * GETで受け取った2つの変数とsample22()の引数の名前が同じなため， 引数の前に @PathVariable と付けるだけで，パスパラメータの値を
+   * javaで処理できるようになる ModelMapはthymeleafに渡すためのMapと呼ばれるデータ構造を持つ変数
+   * Mapはkeyとvalueの組み合わせで値を保持する
+   *
+   * @param param1
+   * @param param2
+   * @param model
+   * @return
+   */
+
+}
