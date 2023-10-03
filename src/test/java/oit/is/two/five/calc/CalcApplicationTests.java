824d3ff6275127f633a522f465cd5c99be4a398c
e1b21073 <e1b21073@oit.ac.jp>
2023-10-03 14:39:19 +0900

    リポジトリの初期化

---
+++ src/test/java/oit/is/two/five/calc/CalcApplicationTests.java
@@ -0,0 +1,13 @@
+package oit.is.two.five.calc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
